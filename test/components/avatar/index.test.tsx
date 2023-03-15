3e31f103bd8df714a45cb71c58b50d6e24a61483
Ibrahim Rahhal <ibrahim.rahhal3636@gmail.com>
2023-03-15 14:25:37 +0300

    UI Improvments/ More Unit tests

---
+++ test/components/avatar/index.test.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Avatar } from '@/components/avatar';
+import styles from '@/components/avatar/index.module.scss';
+
+describe('Components', () => {
+    describe('Avatar', () => {
+        it('should render an image with the specified src and alt', () => {
+            render(<Avatar src="avatar.jpg" alt="Avatar" />);
+            const image = screen.getByAltText('Avatar');
+            expect(image).toHaveAttribute('src', 'avatar.jpg');
+        });
+
+        it('should render the correct size class when a size prop is provided', () => {
+            render(<Avatar src="avatar.jpg" alt="Avatar" size="small" />);
+            const avatar = screen.getByRole('img').closest('div');
+            expect(avatar).toHaveClass(styles['avatar--small']);
+        });
+
+        it('should add any additional class names provided', () => {
+            render(
+                <Avatar
+                    src="avatar.jpg"
+                    alt="Avatar"
+                    className="custom-class"
+                />
+            );
+            const avatar = screen.getByRole('img').closest('div');
+            expect(avatar).toHaveClass(styles.avatar, 'custom-class');
+        });
+
+        it('should pass through any additional props to the container div', () => {
+            const onClick = jest.fn();
+            render(
+                <Avatar
+                    src="avatar.jpg"
+                    alt="Avatar"
+                    onClick={onClick}
+                    style={{
+                        color: 'blue'
+                    }}
+                />
+            );
+            const avatar = screen.getByRole('img').closest('div');
+            expect(avatar).toHaveAttribute('style');
+            if (avatar) avatar.click();
+            expect(onClick).toHaveBeenCalled();
+        });
+    });
+});
--- test/components/button/index.test.tsx
+++ test/components/button/index.test.tsx
@@ -1,13 +1,57 @@
-import { render, screen,  } from '@testing-library/react';
-import {Button} from '@/components/button';
-import '@testing-library/jest-dom';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Button } from '@/components/button';
+import styles from '@/components/button/index.module.scss';
 
 describe('Components', () => {
     describe('Button', () => {
-        it('renders a button', () => {
-            render(<Button>Test</Button>);
-            const button = screen.getByRole('button')
-            expect(button).toBeInTheDocument()
-        })
+        it('should render the children', () => {
+            render(<Button>Click me!</Button>);
+            expect(screen.getByText('Click me!')).toBeInTheDocument();
+        });
+
+        it('should apply the correct color class', () => {
+            render(<Button color="secondary">Click me!</Button>);
+            const button = screen.getByRole('button');
+            expect(button).toHaveClass(
+                styles.button,
+                styles['button--secondary']
+            );
+        });
+
+        it('should apply the correct size class', () => {
+            render(<Button size="large">Click me!</Button>);
+            const button = screen.getByRole('button');
+            expect(button).toHaveClass(styles.button, styles['button--large']);
+        });
+
+        it('should apply the correct variant class', () => {
+            render(<Button variant="outlined">Click me!</Button>);
+            const button = screen.getByRole('button');
+            expect(button).toHaveClass(
+                styles.button,
+                styles['button--outlined']
+            );
+        });
+
+        it('should call the onClick function when the button is clicked', () => {
+            const onClick = jest.fn();
+            render(<Button onClick={onClick}>Click me!</Button>);
+            const button = screen.getByRole('button');
+            if (button) button.click();
+            expect(onClick).toHaveBeenCalled();
+        });
+
+        it('should pass through any additional props to the button element', () => {
+            render(<Button data-testid="test-button">Click me!</Button>);
+            const button = screen.getByTestId('test-button');
+            expect(button).toBeInTheDocument();
+        });
+
+        it('should add any additional class names provided', () => {
+            render(<Button className="custom-class">Click me!</Button>);
+            const button = screen.getByRole('button');
+            expect(button).toHaveClass(styles.button, 'custom-class');
+        });
     });
 });
